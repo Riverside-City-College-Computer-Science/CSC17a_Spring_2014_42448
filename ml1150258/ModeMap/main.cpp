561807725b4ffe4d0cd5192e7ced4bd60d19c2da
mlehrjava <mark.lehr@rcc.edu>
2014-03-06 19:49:28 -0800

    Map and Sets
    Mode derived from the STL Map and Sets

--- ml1150258/ModeMap/main.cpp
+++ ml1150258/ModeMap/main.cpp
@@ -1,12 +1,13 @@
-/* 
- * File:   main.cpp
- * Author: Dr. Mark E. Lehr
- * Created on March 5, 2014
- */
+/*
+* File: main.cpp
+* Author: Dr. Mark E. Lehr
+* Created on March 5, 2014
+*/
 
 //System Libraries
 #include <iostream>
 #include <map>
+#include <set>
 using namespace std;
 
 //Global Constants
@@ -15,21 +16,69 @@ using namespace std;
 void fillAry(int [], int);
 void prntAry(int [], int, int);
 int maxFreq(int [], int, map<int,int> &);
-int nmModes(int, map<int,int> &);
+int nmModes(int, map<int,int> &, set<int> &);
+void prntMds(int,int,set<int> &);
+void fndMods(int [],int,int &,int &,set<int> &);
 
 int main(int argc, char** argv) {
     //Declare some variables
-    const int SIZE=105;
+    const int SIZE=106;
     int array[SIZE];
-    map<int,int> freq;
+    set<int> modes;
+    int mxFreq,nModes;
     //Fill the array with 0-9
     fillAry(array,SIZE);
     //Print the array
     prntAry(array,SIZE,10);
-    //What is the max frequency
-    cout<<"The max freq in the array = "<<maxFreq(array,SIZE,freq)<<endl;
+    //Find the modes
+    fndMods(array,SIZE,mxFreq,nModes,modes);
+    //Print the Mode Set
+    prntMds(mxFreq,nModes,modes);
     //Exit stage right!
     return 0;
+}
+
+void fndMods(int a[],int n,int &mFreq,int &nModes,set<int> &modes){
+    //Declare the map
+    map<int,int> freq;
+    //What is the max frequency
+    mFreq=maxFreq(a,n,freq);
+    //How many modes?
+    if(mFreq>1)nModes=nmModes(mFreq,freq,modes);
+    else nModes=0;
+}
+
+void prntMds(int mxFreq,int nModes,set<int> &s){
+    //Print the max frequency
+    cout<<"The max frequency in the array = "<<mxFreq<<endl;
+    //Print the number of modes
+    cout<<"The number of modes = "<<nModes<<endl;
+    //Start a counter for a proper
+    if(nModes>0){
+        typedef set<int>::iterator its;
+        cout<<"The mode set = {";
+        for(its iterator = s.begin(); iterator != s.end(); iterator++) {
+                if(nModes-->1)cout<<*iterator<<",";
+                else cout<<*iterator;
+        }
+        cout<<"}"<<endl;
+    }
+}
+
+//Count the modes by utilize the map and max frequency
+int nmModes(int max, map<int,int> &m, set<int> &s){
+    //Set counter for the modes
+    max;//frequency in map is 1 less
+    int nModes=0;
+    //For each element in the map, see if it matches the max frequency
+    typedef map<int,int>::iterator itm;
+    for(itm iterator = m.begin(); iterator != m.end(); iterator++) {
+        if(iterator->second==max){
+            s.insert(iterator->first);
+            nModes++;
+        }
+    }
+    return nModes;
 }
 
 //No Sorting Required
@@ -39,9 +88,9 @@ int maxFreq(int a[],int n,map<int,int>&m){
     int max=0;
     //Loop and compare
     for(int i=0;i<n;i++){
-        if(max<m[a[i]]++)max=m[a[i]];
+        if(++m[a[i]]>max)max=m[a[i]];
     }
-    return max+1;
+    return max;
 }
 
 void prntAry(int a[],int n,int perLine){
@@ -53,10 +102,9 @@ void prntAry(int a[],int n,int perLine){
     cout<<endl;
 }
 
-//Repetitive Count with 1 digit elements 
+//Repetitive Count with 1 digit elements
 void fillAry(int a[],int n){
     for(int i=0;i<n;i++){
         a[i]=i%10;
     }
 }
-
