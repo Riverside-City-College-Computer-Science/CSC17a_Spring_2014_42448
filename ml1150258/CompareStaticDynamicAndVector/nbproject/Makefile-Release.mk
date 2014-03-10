f707ef1420d043f8c186a59b59ab56440d2d4bd2
mlehrjava <mark.lehr@rcc.edu>
2014-03-10 14:07:00 -0700

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

--- ml1150258/CompareStaticDynamicAndVector/nbproject/Makefile-Release.mk
+++ ml1150258/CompareStaticDynamicAndVector/nbproject/Makefile-Release.mk
@@ -22,7 +22,7 @@ AS=as
 
 # Macros
 CND_PLATFORM=Cygwin_4.x-Windows
-CND_CONF=Debug
+CND_CONF=Release
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 
@@ -55,16 +55,16 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/modemap.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/comparestaticdynamicandvector.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/modemap.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/comparestaticdynamicandvector.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/modemap ${OBJECTFILES} ${LDLIBSOPTIONS} 
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/comparestaticdynamicandvector ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
@@ -72,7 +72,7 @@ ${OBJECTDIR}/main.o: main.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/modemap.exe
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/comparestaticdynamicandvector.exe
 
 # Subprojects
 .clean-subprojects:
