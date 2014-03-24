9aeaf244152f407384696593d569b0a22f7c53a8
mlehrjava <mark.lehr@rcc.edu>
2014-03-24 15:15:22 -0700

    Array Structures
    Arrays with 1 and 2 dimensional arrays

---
+++ ml1150258/ArrayStructures/AryStruc.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   AryStruc.h
+ * Author: rcc
+ *
+ * Created on March 24, 2014, 1:21 PM
+ */
+
+#ifndef ARYSTRUC_H
+#define	ARYSTRUC_H
+
+struct AryStruc1{
+    int *array;
+    int size;
+};
+
+struct AryStruc2{
+    int **array;
+    int records;
+    int fields;
+};
+
+#endif	/* ARYSTRUC_H */
+
