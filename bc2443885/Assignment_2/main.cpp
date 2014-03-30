939e1dd9857b09ad86e1af6a9bb1bb0aff8b463e
radbradd <braddley.carey@gmail.com>
2014-03-29 18:52:03 -0700

    Started assignment 2
    program 1 finished

---
+++ bc2443885/Assignment_2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rente_000
+ *
+ * Created on March 29, 2014, 6:51 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
