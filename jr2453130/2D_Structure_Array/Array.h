af82fd63572f680dc902eaa579116d381f6970dd
HalvallaAwaits <joereim@gmail.com>
2014-03-31 19:59:45 -0700

    Assignment2 and 2d structure array

---
+++ jr2453130/2D_Structure_Array/Array.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   Array.h
+ * Author: Dr. Mark E. Lehr
+ * Created on September 23, 2013, 8:24 PM
+ */
+
+#ifndef ARRAY_H
+#define	ARRAY_H
+
+struct Array{
+    unsigned int size;
+    int *data;
+};
+
+#endif	/* ARRAY_H */
+
