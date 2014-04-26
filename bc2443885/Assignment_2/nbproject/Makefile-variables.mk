b7c8d905da5e59a970d466166405507188d4e90d
zhangyi123 <376125280@qq.com>
2014-04-26 15:00:43 +0800

    Signed-off-by: zhangyi123 <376125280@qq.com>

--- bc2443885/Assignment_2/nbproject/Makefile-variables.mk
+++ bc2443885/Assignment_2/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=assignment_2
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/assignment_2
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/assignment_2
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=assignment2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/assignment2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/assignment2.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=assignment_2
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/assignment_2
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/assignment_2
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=assignment2.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/assignment2.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/assignment2.tar
 #
 # include compiler specific variables
 #
